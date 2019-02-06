d4c99665c1bfe3a460dd114547fee1c2c22d74c3
TimeApollo <arnjcksn0845@yahoo.com>
2019-02-06 15:26:31 -0500

    integrated slack and twitter bots together.

--- slack.py
+++ slack.py
@@ -7,27 +7,37 @@ import signal
 from slackclient import SlackClient
 from functools import partial
 import logging
+import twitbot
+import pprint
+
+logger = logging.getLogger(os.path.basename(__file__))
+pp = pprint.PrettyPrinter(indent=2)
 
 exit_flag = False
 started = False
+subscr = []
 
 bot_commands = {
     'help': 'returns list of commands that can be used',
-    'add': 'adds topic to search for on twitter',
+    'add': 'adds subscription to search for on twitter',
     'ping': 'shows uptime of bot',
     'exit': 'stops twitter and slack bots.',
-    'start': 'starts twitter bot listening to topic list',
-    'time': 'starts slack bot.'
+    'start': 'starts twitter bot subscription list(ex: start python piero)',
+    'time': 'starts slack bot.',
+    'remove': 'removes subscription from list',
+    'removeall': 'removes all subscriptions',
+    'list': 'shows current subscriptions',
+    'stop': 'stops current twitter stream (subscriptions not deleted)',
+    'channels': 'to check all available channels'
 }
 
 
 class slack_bot(SlackClient):
     """slack_bot class."""
-    def __init__(self, token, channel, logger, start_time, bot_id=None):
+    def __init__(self, token, channel, bot_id=None):
         self.sc = SlackClient(token)
         self.channel = channel
-        self.logger = logger
-        self.start_time = start_time
+        self.start_time = datetime.datetime.now()
         self.bot_id = bot_id
         if not self.bot_id and self.sc.rtm_connect(with_team_state=False):
             response = self.sc.api_call('auth.test')
@@ -38,14 +48,16 @@ class slack_bot(SlackClient):
     def __enter__(self):
         """returns slack obj and connects to rtm if not."""
         if self.sc.server.connected:
+            logger.info('SlackBot connected to rtm stream')
             return self
         else:
+            logger.info('SlackBot connected to rtm stream')
             self.sc.rtm_connect(with_team_state=False)
             return self
 
     def __exit__(self, type, value, traceback):
         """lets program know that it is exiting slackbot."""
-        self.logger.info('Exiting slack_bot')
+        logger.info('Exiting slack_bot')
 
     def read_stream(self):
         """reads stream from slack_client connections."""
@@ -53,6 +65,7 @@ class slack_bot(SlackClient):
 
     def parse_stream(self, content):
         """takes in content from stream and looks for pbjtime mentions."""
+        # logger.info(content)
         for item in content:
             if 'text' in item and item['text'].startswith(self.at_bot):
                 text = item['text'].split(self.at_bot)
@@ -60,34 +73,73 @@ class slack_bot(SlackClient):
                 return (text[1].strip(), chan)
         return (None, None)
 
-    def handle_command(self, text):
+    def handle_command(self, text, tb):
         """handles commands that are given and returns message to post."""
         global started
-        args = text.split()
+        global subscr
+        args = text.lower().split()
         if args:
             cmd = args[0].lower()
+            logger.info('{} cmd was issued.')
         else:
             cmd = ''
+        args = args[1:]
         if not started and cmd != 'time':
-            return 'Peanut? Butter? Jelly?!? Time?!?!?!?!?!? (use time cmd to start pbjtime)'
-        if cmd not in bot_commands:
+            return 'Peanut? Butter? Jelly?!? Time?!?!? (time cmd to start me)'
+        elif cmd not in bot_commands:
             return 'Peanut Butter Jelly Time??? use help for more options.'
-        if cmd == 'help':
+        elif cmd == 'help':
             return 'these commands are possible: {}'.format(bot_commands)
-        if cmd == 'time':
+        elif cmd == 'time':
             started = True
-            return "IT'S PEANUT BUTTER JELLY TIME!!!!!!!!!! \n (help for more options)"
-        if cmd == 'ping':
+            return "IT'S PEANUT BUTTER JELLY TIME!! \n(help for more options)"
+        elif cmd == 'ping':
             uptime = datetime.datetime.now() - self.start_time
             return 'Peanut Butter Jelly upTime: {}'.format(uptime)
-        if cmd == 'exit':
+        elif cmd == 'exit':
             started = False
+            tb.close_stream()
+            subscr = []
+            tb.subscriptions = []
             return "peanut butter jelly time :'( (goodbye)"
-        if cmd == 'start':
-            return 'command does not work yet'
-        if cmd == 'add':
-            return 'command does not work yet'
-        return None
+        elif cmd == 'start':
+            subscr = list(set(subscr + args))
+            if not subscr:
+                return 'Please add subscriptions so I can find tweets.'
+            tb.init_stream(subscr)
+            if args:
+                return 'Added subscriptions: {}'.format(args)
+            return 'Started with subcriptions: {}'.format(subscr)
+        elif cmd == 'add':
+            subscr = list(set(subscr + args))
+            if not subscr or not args:
+                return 'Please add new subscriptions so I can find tweets.'
+            tb.init_stream(subscr)
+            return 'Added subscriptions: {}'.format(args)
+        elif cmd == 'remove':
+            removed = []
+            for arg in args:
+                if arg in subscr:
+                    subscr.remove(arg)
+                    removed.append(arg)
+            tb.init_stream(subscr)
+            return 'removed subcriptions: {} and restarted.'.format(args)
+        elif cmd == 'removeall':
+            subscr = []
+            tb.close_stream()
+            tb.subscriptions = []
+            return 'all subscriptions removed!'
+        elif cmd == 'list':
+            return 'current subscriptons: \n {}'.format(subscr)
+        elif cmd == 'stop':
+            logger.info('Stopping twitter stream')
+            tb.close_stream()
+            logger.info('stream closed on slack side.')
+            return 'Twitter stream has been stopped.'
+        elif cmd == 'channels':
+            self.channel_list()
+        else:
+            return None
 
     def post_command_message(self, message, channel):
         """posts message after command is completed."""
@@ -95,7 +147,11 @@ class slack_bot(SlackClient):
 
     def post_twit_mess(self, message):
         """Posts message from twitter bot to initial channel."""
-        self.sc.rtm_send_message(self.channel, message)
+        self.sc.api_call("chat.postMessage", channel=self.channel, text=message)
+
+    def channel_list(self):
+        logger.info('requesting channel list')
+        logger.info(pp.pformat(self.sc.api_call("channels.list")))
 
 
 def exit_logger(logger, app_start_time):
@@ -125,7 +181,6 @@ def init_logger(logger, start_time):
 
 def create_logger():
     """Creates logger for program."""
-    logger = logging.getLogger(os.path.basename(__file__))
     logger.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter(
@@ -141,7 +196,6 @@ def create_logger():
 
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
-    return logger
 
 
 def sig_handler(logger, sig_num, frame):
@@ -158,7 +212,7 @@ def sig_handler(logger, sig_num, frame):
 
 def main():
 
-    logger = create_logger()
+    create_logger()
     # start time
     app_start_time = datetime.datetime.now()
     # make  beginning banner
@@ -173,15 +227,18 @@ def main():
     ch = os.getenv('CHANNEL')
     bi = os.getenv('BOT_ID')
 
-    with slack_bot(st, ch, logger, app_start_time, bot_id=bi) as sb:
-        while not exit_flag:
-            stream = sb.read_stream()
-            text, chan = sb.parse_stream(stream)
-            if text is not None and chan:
-                message = sb.handle_command(text)
-                if message:
-                    sb.post_command_message(message, chan)
-            time.sleep(1)
+    with slack_bot(st, ch, bot_id=bi) as sb:
+        with twitbot.WatchTwitter() as tb:
+            twitbot.init_logger()
+            tb.register_slack(sb.post_twit_mess)
+            while not exit_flag:
+                stream = sb.read_stream()
+                text, chan = sb.parse_stream(stream)
+                if text is not None and chan:
+                    message = sb.handle_command(text, tb)
+                    if message:
+                        sb.post_command_message(message, chan)
+                time.sleep(1)
 
     exit_logger(logger, app_start_time)
     return 0
---
+++ twitbot.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Travis Anderson"
+
+"""
+This is for contacting twitter, and watching a specific user or word
+
+"""
+import logging
+import tweepy
+import time
+import os
+import datetime
+from threading import Thread
+import threading
+
+logger = logging.getLogger(os.path.basename(__file__))
+exit_flag = False
+
+
+def _start(self, is_async):
+    """Monkey patch to allow multi threading so twitter can run and
+    main program can run"""
+    self.running = True
+    if is_async:
+        logger.warning("Initiating multithread")
+        self._thread = Thread(
+            target=self._run, name="Tweepy Thread", daemon=True)
+        self._thread.start()
+    else:
+        self._run()
+
+
+class WatchTwitter(tweepy.StreamListener):
+    """Class that subscribes to keywords on twitter """
+
+    def __init__(self):
+        logger.info("Creating api")
+        consumer_key = os.getenv("API_KEY")
+        assert consumer_key is not None
+        consumer_secret = os.getenv("API_SECRET")
+        access_token = os.getenv("ACCESS_TOKEN")
+        access_token_secret = os.getenv("ACCESS_SECRET")
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        self.api = tweepy.API(auth)
+        tweepy.Stream._start = _start
+        self.subscriptions = []
+        self._stop_event = threading.Event()
+        self.stream_timestamp = 0
+        self.master_timestamp = 0
+        self.register = None
+        self.stream = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if self.stream.running:
+            self.close_stream()
+
+    def add_subscription(self, subscribe_to):
+        """If stream is running adds new subscription, restarts stream"""
+        if subscribe_to not in self.subscriptions:
+            logger.info('Adding subscription: {}'.format(subscribe_to))
+            self.subscriptions.append(subscribe_to)
+            logger.info(self.subscriptions)
+            self.stream.running = False
+            self.start_stream()
+        else:
+            logger.info("Already subscribed: {}" .format(self.subscriptions))
+
+    def remove_subscription(self, unsubscribe_from):
+        logger.info("Attempting to remove {}".format(unsubscribe_from))
+        if unsubscribe_from in self.subscriptions:
+            logger.info(
+                'Removing from subscriptions: {}'.format(unsubscribe_from))
+            self.subscriptions.remove(unsubscribe_from)
+            self.stream.running = False
+            self.start_stream()
+
+    def pause_stream(self):
+        if self.stream.running:
+            logger.info("Pausing all subscriptions: {}".format(
+                self.subscriptions))
+            self.stream.running = False
+
+    def restart_stream(self):
+        if not self.stream.running:
+            logger.info("Restarting stream")
+            self.start_stream()
+
+    def close_stream(self):
+        logger.info('Closing Stream')
+        self.stream.disconnect()
+        self.stream.running = False
+        logger.info('Stream Closed')
+
+    def init_stream(self, new_subscriptions):
+        self.subscriptions = new_subscriptions
+        self.start_stream()
+
+    def start_stream(self):
+        global exit_flag
+        exit_flag = False
+        if self.stream:
+            self.close_stream()
+        logger.info('Subscriptions: {}'.format(self.subscriptions))
+        self.stream = tweepy.Stream(auth=self.api.auth, listener=self)
+        self.stream.filter(track=self.subscriptions, is_async=True)
+
+    def on_status(self, status):
+        # need a stream handler, if not none run the stream handler and
+        # send the status to slack, else return not exit flag
+        if self.register:
+            if not status.text.startswith('RT'):
+                # logger.info(status.text)
+                self.register(status.text)
+        else:
+            logger.info("not registered yet")
+
+    def register_slack(self, slackfunction):
+        self.register = slackfunction
+
+    def on_connect(self):
+        self.stream_timestamp = datetime.datetime.now()
+        logger.info('Connected to twitter at: {}'.format(
+            datetime.datetime.now()))
+        if not self.master_timestamp:
+            self.master_timestamp = self.stream_timestamp
+
+
+def log_config():
+    """Adjusts how info is displayed in log"""
+    return logging.basicConfig(
+        format=(
+            '%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s '
+            '[%(threadName) -12s] %(message)s'),
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+
+def log_set_level():
+    """Sets defaulf log level"""
+    logger.setLevel(logging.DEBUG)
+
+
+def init_logger():
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s.%(msecs)03d %(name)-12s \
+        %(levelname)-8s [%(threadName)-12s] %(message)s'
+    )
+
+    file_handler = logging.FileHandler('slacktweet.log')
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+
+def main():
+    global exit_flag
+    log_config()
+    log_set_level()
+
+    tb = WatchTwitter()
+    tb.init_stream('python')
+    while not exit_flag:
+        time.sleep(5)
+        tb.pause_stream()
+        time.sleep(5)
+        tb.add_subscription('Trump')
+        time.sleep(5)
+        tb.remove_subscription('Trump')
+
+
+if __name__ == "__main__":
+    main()
+    pass
